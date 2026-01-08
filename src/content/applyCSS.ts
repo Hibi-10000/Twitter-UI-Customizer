986787bb960244f9c3671830f1db529d8b4d1ef5
Hibi_10000 <Hibi-10000@outlook.jp>
2026-01-08 23:32:48 +0900

    不要なチェックを消す

--- src/content/applyCSS.ts
+++ src/content/applyCSS.ts
@@ -128,32 +128,29 @@ export function applySystemCss() {
     }
     document.documentElement.dataset.tuicSettings = settingsOutput;
 
-    const r = document.documentElement;
-    if (r instanceof HTMLElement) {
-        const rs = r.style;
+    const rs = document.documentElement.style;
 
-        for (const elem of getSettingIDs("buttonColor")) {
-            for (const el of ["background", "border", "color"]) {
-                if (ColorData.defaultTUICColor.colors[elem][el]) {
-                    rs.setProperty(`--twitter-${elem}-${el}`, getColorFromPref(elem, el, null));
-                }
+    for (const elem of getSettingIDs("buttonColor")) {
+        for (const el of ["background", "border", "color"]) {
+            if (ColorData.defaultTUICColor.colors[elem][el]) {
+                rs.setProperty(`--twitter-${elem}-${el}`, getColorFromPref(elem, el, null));
             }
         }
-        rs.setProperty("--twitter-TUIC-color", ColorData.TUICFixedColor[backgroundColor].textColor);
-        rs.setProperty("--TUIC-container-background", ColorData.TUICFixedColor[backgroundColor].containerBackground);
-        rs.setProperty("--TUIC-container-background2", ColorData.TUICFixedColor[backgroundColor].containerBackground2);
-        rs.setProperty("--TUIC-color-hover-efect", ColorData.TUICFixedColor[backgroundColor].colorHover);
-
-        rs.setProperty("--TUIC-sidebar-hover-color", backgroundColorCheck() == "light" ? "rgba(15,20,25,0.1)" : "rgba(247,249,249,0.1)");
-        rs.setProperty("--TUIC-sidebar-active-color", backgroundColorCheck() == "light" ? "rgba(15,20,25,0.2)" : "rgba(247,249,249,0.2)");
-        rs.setProperty("--TUIC-sidebar-focus-color", backgroundColorCheck() == "light" ? "rgb(135,138,140)" : "rgb(251,252,252)");
-
-        rs.setProperty("--TUIC-detail-border", ColorData.TUICFixedColor[backgroundColor].detailBorder);
-
-        rs.setProperty("--TUIC-pinnedTab-background", `rgba(${backgroundColorClass("0, 0, 0, 0.65", "21, 32, 43, 0.75", "255, 255, 255, 0.85")})`);
-
-        rs.setProperty("--TUIC-pinnedTab-top", `${fontSizeClass("47", "49", "52", "57", "63")}px`);
     }
+    rs.setProperty("--twitter-TUIC-color", ColorData.TUICFixedColor[backgroundColor].textColor);
+    rs.setProperty("--TUIC-container-background", ColorData.TUICFixedColor[backgroundColor].containerBackground);
+    rs.setProperty("--TUIC-container-background2", ColorData.TUICFixedColor[backgroundColor].containerBackground2);
+    rs.setProperty("--TUIC-color-hover-efect", ColorData.TUICFixedColor[backgroundColor].colorHover);
+
+    rs.setProperty("--TUIC-sidebar-hover-color", backgroundColorCheck() == "light" ? "rgba(15,20,25,0.1)" : "rgba(247,249,249,0.1)");
+    rs.setProperty("--TUIC-sidebar-active-color", backgroundColorCheck() == "light" ? "rgba(15,20,25,0.2)" : "rgba(247,249,249,0.2)");
+    rs.setProperty("--TUIC-sidebar-focus-color", backgroundColorCheck() == "light" ? "rgb(135,138,140)" : "rgb(251,252,252)");
+
+    rs.setProperty("--TUIC-detail-border", ColorData.TUICFixedColor[backgroundColor].detailBorder);
+
+    rs.setProperty("--TUIC-pinnedTab-background", `rgba(${backgroundColorClass("0, 0, 0, 0.65", "21, 32, 43, 0.75", "255, 255, 255, 0.85")})`);
+
+    rs.setProperty("--TUIC-pinnedTab-top", `${fontSizeClass("47", "49", "52", "57", "63")}px`);
 }
 
 export function applyCustomCss() {
