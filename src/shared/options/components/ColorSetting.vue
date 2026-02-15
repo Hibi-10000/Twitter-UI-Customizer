c546dff5bea8dfc05136d02b63d45ed6842f2e6f
Hibi_10000 <Hibi-10000@outlook.jp>
2026-02-16 07:40:40 +0900

    refactor: 設定ページのCSSをVue SFCに移動 (Part 4)

--- src/shared/options/components/ColorSetting.vue
+++ src/shared/options/components/ColorSetting.vue
@@ -123,4 +123,29 @@ function changeColorCheck(colorAttr, colorType, colorKind, isChecked) {
 .TUIC_ISNOTDEFAULT :deep(button.TUICDefaultColor) {
     cursor: not-allowed;
 }
+
+/* 色選択 */
+.TUIC_setting_color_colmn {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+}
+
+.TUIC_setting_color_colmn > h4 {
+    margin: 0 !important;
+    font-size: 15px;
+    font-weight: 700 !important;
+    color: color-mix(in srgb, var(--TUIC-container-background), var(--twitter-TUIC-color) 40%);
+}
+
+.TUIC_setting_color_colmn > .TUIC_setting_input_container {
+    display: flex;
+    gap: 10px;
+}
+
+/* 色設定項目の操作可能なオブジェクトのコンテナ */
+.TUIC_setting_input_container {
+    display: flex;
+    align-items: center;
+}
 </style>
